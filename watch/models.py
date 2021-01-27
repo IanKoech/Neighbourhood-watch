37b84ff5bdc19d4c0aafe3b6bfcbdf95cdba9465
FGacheru <francisgacheru2001@gmail.com>
2021-01-27 23:47:11 +0300

    add profile class on the models template

--- watch/models.py
+++ watch/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+import datetime as dt
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Profile(models.Model):
+    profile_pic= models.ImageField(upload_to='profile/' )
+    bio = models.TextField(blank=True)
+    more_info = models.TextField(blank=False, null=True)
+    user = models.ForeignKey(User, null= True, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.bio
+    
+    def save_user(self):
+        self.save()
--- watch/views.py
+++ watch/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import *
 from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 @login_required(login_url='login')
