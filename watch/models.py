55c4bd3daf4ca6d1ff1aa612f2311ea3589425a6
FGacheru <francisgacheru2001@gmail.com>
2021-01-27 23:50:56 +0300

    add Neighbourhood class on the models template

--- watch/models.py
+++ watch/models.py
@@ -6,13 +6,41 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Profile(models.Model):
-    profile_pic= models.ImageField(upload_to='profile/' )
-    bio = models.TextField(blank=True)
-    more_info = models.TextField(blank=False, null=True)
-    user = models.ForeignKey(User, null= True, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=80, blank=True)
+    bio = models.TextField(max_length=254, blank=True)
+    profile_picture = models.ImageField(upload_to='images/', default='default.png')
+    location = models.CharField(max_length=50, blank=True, null=True)
+    neighbourhood = models.ForeignKey('Neighbourhood', on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
     
     def __str__(self):
         return self.bio
     
     def save_user(self):
         self.save()
+        
+
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=60)
+    admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
+    neighbourhood_logo = models.ImageField(upload_to='images/')
+    description = models.TextField()
+    healthcenter_number = models.IntegerField(null=True, blank=True)
+    police_number = models.IntegerField(null=True, blank=True)
+    occupants_count = models.IntegerField(null=True, blank=True)
+
+
+    def __str__(self):
+        return f'{self.name} hood'
+
+    def save_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+    @classmethod
+    def find_neighborhood(cls, neighborhood_id):
+        return cls.objects.filter(id=neighborhood_id)
+    
