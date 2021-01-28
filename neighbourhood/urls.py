7062538fbfd9354fc51a712f79a4682b3e775c93
FGacheru <francisgacheru2001@gmail.com>
2021-01-28 23:40:04 +0300

    add authentication urls path in the project url

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('',include('watch.urls')),
+    url('accounts/', include('django_registration.backends.one_step.urls')),
+    url('accounts/', include('django.contrib.auth.urls')),
+    url("logout", views.logout_then_login, name= "logout"),
 ]
